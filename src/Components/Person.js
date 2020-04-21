b15ed6c367ef2a3ae26b8bffe498b5d12ea4e793
natelloyd1 <nathanaellloyd@live.com>
2020-04-21 12:39:11 +0100

    section 4 done

--- src/Components/Person.js
+++ src/Components/Person.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Person = (props) => {
     return (
         <div>
-        <p onClick={props.click}>I am {props.name} and I am {(Math.floor(Math.random() * 30))} years old</p>
+        <p onClick={props.click}>I am {props.name} and I am {props.age} years old</p>
         <p>{props.children}</p>
-        <input type="text" onChange={props.changed} />
+        <input type="text" onChange={props.changed} value={props.name}/>
         </div>
     )}
 
